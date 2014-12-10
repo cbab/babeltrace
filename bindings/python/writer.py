0a69910cceef9648f13ec70191b26a0188acae7a
Philippe Proulx <eeppeliteloop@gmail.com>
2014-12-09 20:08:02 -0500

    Python: document writer.IntegerFieldDeclaration
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- bindings/python/writer.py
+++ bindings/python/writer.py
@@ -372,17 +372,28 @@ class FieldDeclaration:
 
 
 class IntegerFieldDeclaration(FieldDeclaration):
+    """
+    Integer field declaration.
+    """
+
     def __init__(self, size):
         """
-        Create a new integer field declaration of the given size.
+        Creates an integer field declaration of size *size* bits.
+
+        :exc:`ValueError` is raised on error.
         """
+
         self._ft = nbt._bt_ctf_field_type_integer_create(size)
         super().__init__()
 
     @property
     def size(self):
         """
-        Get an integer's size.
+        Integer's size in bits (integer).
+
+        Set this attribute to change this integer's size.
+
+        :exc:`ValueError` is raised on error.
         """
 
         ret = nbt._bt_ctf_field_type_integer_get_size(self._ft)
@@ -395,7 +406,11 @@ class IntegerFieldDeclaration(FieldDeclaration):
     @property
     def signed(self):
         """
-        Get an integer's signedness attribute.
+        ``True`` if this integer is signed.
+
+        Set this attribute to change this integer's signedness (boolean).
+
+        :exc:`ValueError` is raised on error.
         """
 
         ret = nbt._bt_ctf_field_type_integer_get_signed(self._ft)
@@ -409,10 +424,6 @@ class IntegerFieldDeclaration(FieldDeclaration):
 
     @signed.setter
     def signed(self, signed):
-        """
-        Set an integer's signedness attribute.
-        """
-
         ret = nbt._bt_ctf_field_type_integer_set_signed(self._ft, signed)
 
         if ret < 0:
@@ -421,19 +432,17 @@ class IntegerFieldDeclaration(FieldDeclaration):
     @property
     def base(self):
         """
-        Get the integer's base used to pretty-print the resulting trace.
-        Returns a constant from the FieldDeclaration.IntegerBase class.
+        Integer's display base (one of :class:`IntegerBase` constants).
+
+        Set this attribute to change this integer's display base.
+
+        :exc:`ValueError` is raised on error.
         """
 
         return nbt._bt_ctf_field_type_integer_get_base(self._ft)
 
     @base.setter
     def base(self, base):
-        """
-        Set the integer's base used to pretty-print the resulting trace.
-        The base must be a constant of the FieldDeclarationIntegerBase class.
-        """
-
         ret = nbt._bt_ctf_field_type_integer_set_base(self._ft, base)
 
         if ret < 0:
@@ -442,20 +451,18 @@ class IntegerFieldDeclaration(FieldDeclaration):
     @property
     def encoding(self):
         """
-        Get the integer's encoding (one of the constants of the
-        CTFStringEncoding class).
-        Returns a constant from the CTFStringEncoding class.
+        Integer's encoding (one of
+        :class:`babeltrace.common.CTFStringEncoding` constants).
+
+        Set this attribute to change this integer's encoding.
+
+        :exc:`ValueError` is raised on error.
         """
 
         return nbt._bt_ctf_field_type_integer_get_encoding(self._ft)
 
     @encoding.setter
     def encoding(self, encoding):
-        """
-        An integer encoding may be set to signal that the integer must be printed
-        as a text character. Must be a constant from the CTFStringEncoding class.
-        """
-
         ret = nbt._bt_ctf_field_type_integer_set_encoding(self._ft, encoding)
 
         if ret < 0:
